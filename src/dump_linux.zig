5ab2e467093be5564e9170e7ba7a1d4097b67699
eulegang <eulegang@eulegang.dev>
2020-03-30 23:43:32 -0500

    extracted out some steps

--- src/dump_linux.zig
+++ src/dump_linux.zig
@@ -21,13 +21,26 @@ pub const DumpError = error{
 };
 
 pub fn dump(path: [*]const u8) DumpError!void {
-    const out_fd = STDOUT_FILENO;
+    var fd = try fd_open(path);
+    var size = try size_of(fd);
+    var addr = try map(fd, size);
+
+    defer unmap(addr, size);
+
+    try write_out(STDOUT_FILENO, addr, size);
+}
+
+fn fd_open(path: [*]const u8) DumpError!i32 {
     var fd = @intCast(i32, open(path, O_RDONLY, 0));
 
     if (fd < 0) {
         return error.NoFile;
     }
 
+    return fd;
+}
+
+fn size_of(fd: i32) DumpError!u64 {
     var stat: Stat = undefined;
 
     var status = fstat(fd, &stat);
@@ -36,7 +49,11 @@ pub fn dump(path: [*]const u8) DumpError!void {
         return error.Stat;
     }
 
-    var mresult = @intCast(isize, mmap(null, @intCast(u64, stat.size), PROT_READ, MAP_PRIVATE, fd, 0));
+    return @intCast(u64, stat.size);
+}
+
+fn map(fd: i32, size: usize) DumpError![*]const u8 {
+    var mresult = @intCast(isize, mmap(null, @intCast(u64, size), PROT_READ, MAP_PRIVATE, fd, 0));
 
     if (mresult < 0) {
         return error.Map;
@@ -44,14 +61,20 @@ pub fn dump(path: [*]const u8) DumpError!void {
 
     var addr = @intToPtr([*]const u8, @intCast(usize, mresult));
 
-    defer {
-        _ = munmap(addr, @intCast(u64, stat.size));
-    }
+    return addr;
+}
 
-    var left = stat.size;
+fn unmap(addr: [*]const u8, size: usize) void {
+    _ = munmap(addr, @intCast(u64, size));
+}
+
+fn write_out(fd: i32, addr: [*]const u8, size: u64) DumpError!void {
+    var left = @intCast(i64, size);
+    var cur = addr;
 
     while (left > 0) {
-        const written = write(out_fd, addr, @intCast(u64, left));
+        const written = write(fd, cur, @intCast(u64, left));
         left -= @intCast(i64, written);
+        cur += written;
     }
 }
